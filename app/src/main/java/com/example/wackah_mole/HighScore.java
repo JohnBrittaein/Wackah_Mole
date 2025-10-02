547e6ff026ef8f878f89d219c432594ac0f0f397
Kitrinos <nellie.leaverton@gmail.com>
2025-10-02 17:45:21 -0500

    Added a return button to the high score page.

--- app/src/main/java/com/example/wackah_mole/HighScore.java
+++ app/src/main/java/com/example/wackah_mole/HighScore.java
@@ -1,4 +1,26 @@
 package com.example.wackah_mole;
 
-public class HighScore {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class HighScore extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+            // Init Start Button
+            Button returnbtn = findViewById(R.id.return_btn);
+
+            // Click Listener for Start Button
+            super.onCreate(savedInstanceState);
+
+            returnbtn.setOnClickListener(v -> {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            });
+        }
+
 }
