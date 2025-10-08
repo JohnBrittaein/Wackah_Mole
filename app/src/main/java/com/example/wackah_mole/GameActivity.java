aedf9edcab44e8ae0d589c826a0403b3edbd3297
jdbritt <brittainjohn4@gmail.com>
2025-10-08 10:36:50 -0500

    Mole Count displayed

--- app/src/main/java/com/example/wackah_mole/GameActivity.java
+++ app/src/main/java/com/example/wackah_mole/GameActivity.java
@@ -28,6 +28,7 @@ public class GameActivity extends AppCompatActivity {
     private Map<Integer, MoleViewState> previousMoles = new HashMap<>();
     private GameViewModel gameModel;
     private EditText gameScore;
+    private EditText moleCountText;
     private Drawable angryMole;
     private Drawable normalMole;
 
@@ -44,6 +45,10 @@ public class GameActivity extends AppCompatActivity {
         gameScore = findViewById(R.id.score);
         gameScore.setText("Score: 0");
 
+        // Initialize mole count
+        moleCountText = findViewById(R.id.mole_count);
+        moleCountText.setText("Moles: 1");
+
         // Load Mole drawables, cache to use later
         angryMole = ContextCompat.getDrawable(this, R.drawable.angry_mole);
         normalMole = ContextCompat.getDrawable(this, R.drawable.mole);
@@ -52,7 +57,10 @@ public class GameActivity extends AppCompatActivity {
         hideMoles();
 
         // Observe score updates
-        gameModel.score.observe(this, score -> gameScore.setText("Score:" + score));
+        gameModel.score.observe(this, score -> gameScore.setText("Score: " + score));
+
+        // Observe mole count updates
+        gameModel.moleCount.observe(this, moleCount -> moleCountText.setText("Moles: " + moleCount));
 
         // Observe mole state updates
         gameModel.getMoleStates().observe(this, this::updateMoleViews);
